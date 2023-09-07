4efee7f5547093b192c11fa9b5b714b4ef5bd359
Noronha1612 <inc.691@gmail.com>
2023-09-07 00:30:23 -0300

    feat: readirect to home page if already logged in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { getSessionSSR } from '@/lib/auth/authOptions'
 import { Hero } from './components/Hero'
 import { Welcome } from './components/Welcome'
+import { redirect } from 'next/navigation'
+import { RouteNames } from './home/routes'
 
 export default async function Landing() {
+  const session = await getSessionSSR()
+
+  if (session?.user) {
+    redirect(RouteNames.Home)
+  }
+
   return (
     <div className="grid h-screen grid-cols-[min(50%,700px),1fr] p-4 md:grid-cols-1">
       <Hero />
